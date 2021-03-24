76f43ee85b1856604e25c4a25dd6923358e63c83
Yordan <d_yugioh@abv.bg>
2021-03-24 17:57:40 +0200

    commit 5

---
+++ www/js/students.js
@@ -0,0 +1,48 @@
+const baseSurviceUrl = "https://students-manager.azurewebsites.net/api/students/";
+const username = "guest";
+const password = "guest";
+const base64Auth = btoa(username + ":" + password);
+
+function loadStudents() {
+    $.mobile.loading("show");
+
+    $.ajax({
+        type: "GET",
+        url: baseSurviceUrl,
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("Authorization", "Basic " + base64Auth)
+        },
+        success: function(students) {
+            $.mobile.loading("hide");
+            console.log({ students });
+            displayStudents(students);
+        },
+        error: function() {
+            $.mobile.loading("hide");
+            alert("Error");
+        }
+    });
+
+    function displayStudents(students) {
+        $("#results").empty();
+
+        for (let student of students) {
+            $('#results').append(
+                $('<li>').append(
+                    $('<a>')
+                    .attr('href', '#detailsPage')
+                    .addClass("ui-btn ui-btn-icon-right ui-icon-carat-r")
+                    .click(function() {
+                        setStudentDetails(student);
+                    })
+                    .append(student.fullName)
+                )
+            );
+        }
+    }
+
+    function setStudentDetails(student) {
+        $("#student-name").text(student.fullName);
+        $("#student-faculty-number").text(student.facultyNumber);
+    }
+}
