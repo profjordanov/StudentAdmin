67d0affdaaebaeedffa5dc999d7fd09c5e8d7323
Jordan Jordanov <d_yugioh@abv.bg>
2022-03-15 09:38:28 +0200

    clean up

--- www/js/index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('deviceready', onDeviceReady, false);
-
-function onDeviceReady() {
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    loadStudents();
-    $("#camera-btn").click(getPicutre);
-    $("#addPicture").click(sendPictureRequest);
-}
