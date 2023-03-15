5a303e7b77d67e766fcea07b488d3b087a223e63
Jordan Jordanov <d_yugioh@abv.bg>
2023-03-15 16:02:17 +0200

    Initial commit

--- www/js/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('deviceready', onDeviceReady, false);
-
-function onDeviceReady() {
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    loadStudents();
-
-    $('#camera-btn').click(getPicture);
-    $('#addPicture').click(sendPictureRequest);
-}
